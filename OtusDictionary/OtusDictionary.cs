b486bdc2420c9cecab5dd711af9f45af50316800
Loskutova Marina <mloskutova1589@gmail.com>
2024-10-16 12:24:02 +0300

    deleted _keys array, changed _current index on local variables

--- OtusDictionary/OtusDictionary.cs
+++ OtusDictionary/OtusDictionary.cs
@@ -8,42 +8,37 @@ namespace OtusDictionary
 {
     public class OtusDictionary
     {
-        private int _currentIndex;
         private int _size;
-        private int[] _keys;
         private string[] _values;
         public OtusDictionary()
         {
             _size = 32;
-            _keys = new int[_size];
             _values = new string[_size];
-            _currentIndex = 0;
         }
 
         public void Add(int key, string? value)
         {
             if (value is null) throw new ArgumentNullException("Value must not be null!");
+            if (_values.Contains(value)) throw new Exception($"Value - {value} already exist!");
+            int currentIndex = GetHash(key);
+            if (currentIndex >= _size) Resize(currentIndex);
+            if (_values[currentIndex] != null) currentIndex = GetAnotherIndex(currentIndex);
 
-            if (_keys.Contains(key)) throw new Exception($"{key} already exsist!");
-
-            _currentIndex = GetHash(key);
-
-            if (_currentIndex >= _size) Resize();
-
-            if (_values[_currentIndex] != null) GetAnotherIndex();
-
-            _keys[_currentIndex] = key;
-            _values[_currentIndex] = value;
+            _values[currentIndex] = value;
         }
 
         public string Get(int key)
         {
-            _currentIndex = Array.IndexOf(_keys, key);
-            if (_currentIndex < 0)
+            int currentIndex = GetHash(key);
+            if (currentIndex >= _size)
             {
                 throw new ArgumentOutOfRangeException($"Argument - [{key}], out of range!");
             }
-            return _values[_currentIndex];
+            if (_values[currentIndex] == null)
+            {
+                throw new ArgumentNullException($"Key - [{key}] does not have value!");
+            }
+            return _values[currentIndex];
         }
 
         public int GetHash(int key)
@@ -51,50 +46,35 @@ namespace OtusDictionary
             return key % _size;
         }
 
-        public void Resize()
+        public void Resize(int currentIndex)
         {
             _size = _size * 2;
-            int[] tmpKeys = _keys;
             string[] tmpValues = _values;
-            _keys = new int[_size];
             _values = new string[_size];
 
-            for (int i=0; i < tmpKeys.Length; i++)
+            for (int i=0; i < tmpValues.Length; i++)
             {
-                _currentIndex = GetHash(tmpKeys[i]);
-                _keys[_currentIndex] = tmpKeys[i];
-                _values[_currentIndex] = tmpValues[i];
+                _values[i] = tmpValues[i];
             }
         }
 
-        public int GetAnotherIndex()
+        public int GetAnotherIndex(int currentIndex)
         {
-            _currentIndex = 0;
-            while (_values[_currentIndex] != null)
+            int newIndex = currentIndex;
+            while (_values[newIndex] != null)
             {
-                _currentIndex++;
-                if (_currentIndex >= _size)
+                newIndex++;
+                if (newIndex >= _size)
                 {
-                    Resize();
+                    Resize(newIndex);
                 }
             }
-            return (_currentIndex - 1);
+            return (newIndex - 1);
         }
 
         public string? this[int index]
         {
-            get
-            {
-                if (_size <= index)
-                {
-                    throw new IndexOutOfRangeException();
-                }
-                else 
-                {
-                    if (string.IsNullOrEmpty(_values[index])) throw new Exception($"No value by index - {index}!");
-                    else return _values[index];
-                }
-            }
+            get => Get(index);
             set => Add(index, value);
         }
     }
